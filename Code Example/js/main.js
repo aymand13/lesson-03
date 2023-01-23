ecc1e3b052fbd544cecd5a3b6a9198e4a727fd6d
Ayman Daher <aymandaher52@gmail.com>
2023-01-23 18:33:39 -0500

    played with css and some js

---
+++ Code Example/js/main.js
@@ -0,0 +1,8 @@
+console.log("Script loaded");
+console.log("hello world");
+
+button = document.querySelector("button");
+button.onclick = () => {
+console.log("you clicked me");
+window.location = "https://google.com";
+};
